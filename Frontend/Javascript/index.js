e70439d4bd3fee6bf0997c24b1a62ec10fd1cd0e
simon <simondiril@gmail.com>
2021-09-15 22:56:02 +0200

    bootstrap nav

--- Frontend/Javascript/index.js
+++ Frontend/Javascript/index.js
@@ -16,9 +16,9 @@ fetch ('http://localhost:3000/api/cameras')
 function getListeProduit(data){
     for (produit of data){
         let liste = document.getElementById("liste");
-        liste.innerHTML +=             `<div class="container-produit">
+        liste.innerHTML +=             `<div class="container">
         <div class="col-sm-12 col-md-6 col-lg-6 pb-3  ">
-                <img src="${produit.imageUrl}" class="" alt="${produit.name}">
+                <img src="${produit.imageUrl}" class="img-fluid" alt="${produit.name}">
                 <h3 class="card-title">${produit.name}</h3>
                 <h4 class="card-title price">${produit.price}</h4>
                 <a class="btn" href="./Frontend/produit.html?_id=${produit._id}"><button class="btnMore"> Acheter ce produits </button></a>
